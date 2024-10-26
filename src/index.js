a94955f9cf6bae8899b34e21ce7bbf14080bdbc8
GreyDraco <ancientgreydragon@gmail.com>
2024-10-26 22:59:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = n.toString();
+    return +(str.charAt(0) === "-"
+        ? str.slice(1).split("").reverse().join("")
+        : str.split("").reverse().join(""));
+};
